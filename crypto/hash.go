10fde0f4e760f579680d01e12f7ac21a49263cfe
Diego Marquina <diegomh210@gmail.com>
2024-11-09 10:05:15 -0500

    Fix hashing cost

--- crypto/hash.go
+++ crypto/hash.go
@@ -4,7 +4,7 @@ import "golang.org/x/crypto/bcrypt"
 
 func HashPassword(password string) (string, error) {
 	// Setting up a reference cost value
-	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 6)
 	return string(bytes), err
 }
 
